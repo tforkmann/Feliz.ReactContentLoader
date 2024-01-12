7be73511e9035efe8fb88c0bd8533e95cda2f7d7
Tim Forkmann <tim.forkmann@msu-solutions.de>
2024-01-12 13:21:45 -0500

    add more text

--- src/Docs/Pages/Use.fs
+++ src/Docs/Pages/Use.fs
@@ -6,15 +6,12 @@ open Docs.SharedView
 
 [<ReactComponent>]
 let UseView () =
-    React.fragment [
-        Html.divClassed "description" [ Html.text "After installation just open proper namespace:" ]
-        Html.divClassed "max-w-xl" [ linedMockupCode "open Feliz.ReactContentLoader" ]
-        Html.divClassed
-            "description"
-            [ Html.text "Now you can start using library. Everything important starts with "
-              Html.code [
-                  prop.className "code"
-                  prop.text "ReactContentLoader.*"
-              ]
-              Html.text " module." ]
-    ]
+    React.fragment
+        [ Html.divClassed "description" [ Html.text "After installation just open proper namespace:" ]
+          Html.divClassed "max-w-xl" [ linedMockupCode "open Feliz.ReactContentLoader" ]
+          Html.divClassed
+              "description"
+              [ Html.text "Now you can start using library. Everything important starts with "
+                Html.code [ prop.className "code"; prop.text "ReactContentLoader.*" ]
+                Html.text " module."
+                Html.text "ReactContentLoader supports two ways of creating loaders." ] ]
